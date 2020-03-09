05e297556dfc4c8601859aeeefa6eaa14d0db31c
SEBASTIAN CAMILO CORTES GOMEZ <scortg@emtelco.com.co>
2020-03-08 19:27:23 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Proyecto Excelsis note TFC
---
+++ src/apptfc/AppTFC.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apptfc;
+
+/**
+ *
+ * @author bosorio
+ */
+public class AppTFC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
